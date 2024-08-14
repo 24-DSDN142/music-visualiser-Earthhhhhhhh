6b86e61a37e704d884e8e9dc953aa8246cd178ce
Earthhhhhhhh <earthsandoval05@gmail.com>
2024-08-15 01:45:02 +1200

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -6,38 +6,51 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   rectMode(CENTER)
   textSize(24);
   
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
+  fill(122, 87, 72);//brown
+  rect(250, 500, 350, 550);
+  fill(43, 41, 40);//black
+  ellipse(250, 500, 300, 300);
+
+  fill(255, 255, 255);
+  let bassSize = map(bass, 0, 300, 150, 600);
+  ellipse(250, 500, bassSize, bassSize);
+  fill(220, 220, 220);
+  let size = map(drum, 0, 300, 75, 400);
+  ellipse(250, 500, size, size);
+
+  
+}
+  //  let bar_spacing = height / 10;
+  //  let bar_height = width / 12;
+  //  let bar_pos_x = width / 2;
  
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+  //  // vocal bar is red
+  //  fill(200, 0, 0);
+  //  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
+  //  fill(0);
+  //  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
+  //  // drum bar is green
+  //  fill(0, 200, 0);
+  //  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
+  //  fill(0);
+  //  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
  
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
+  //  // bass bar is blue
+  //  fill(50, 50, 240);
+  //  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
+  //  fill(0);
+  //  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
  
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
+  //  // other bar is white
+  //  fill(200, 200, 200);
+  //  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
+  //  fill(0);
+  //  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
+  //  fill(255, 255, 0);
  
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
-}
+  //  // display "words"
+  //  textAlign(CENTER);
+  //  textSize(vocal);
+  //  text(words, width/2, height/3);
